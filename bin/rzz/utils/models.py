89627587d0aec0d4ce0e0fae62cebd62d1d3b730
raph <raph@raph-laptop.(none)>
2010-06-01 14:31:59 +0200

    Modified instance_to_json to exclude file fields

--- bin/rzz/utils/models.py
+++ bin/rzz/utils/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields.files import FieldFile
 import json
 
 def instance_to_json(instance, *args, **kwargs):
@@ -9,4 +10,6 @@ def instance_to_json(instance, *args, **kwargs):
         fields = [f.attname for f in instance._meta._fields()]
         return dict([(f, instance.__dict__[f]) for f in fields] + kwargs.items())
 
-    return json.dumps(instance_to_dict(instance))
+    d = instance_to_dict(instance)
+    d = dict([pair for pair in d.items() if type(pair[1]) != FieldFile])
+    return json.dumps(d)
