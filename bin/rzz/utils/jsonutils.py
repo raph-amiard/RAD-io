9ced21ab53ac133806bb0fe89408ef0fc26a4896
raph <raph@raph-laptop.(none)>
2010-06-07 08:10:31 +0200

    Unnested instance_to_dict so it is usable outside of instance_to_json

--- bin/rzz/utils/jsonutils.py
+++ bin/rzz/utils/jsonutils.py
@@ -2,14 +2,14 @@ import json
 from django.db import models
 from django.db.models.fields.files import FieldFile
 
+def instance_to_dict(instance, *args, **kwargs):
+    fields = [f.attname for f in instance._meta._fields()]
+    return dict([(f, instance.__dict__[f]) for f in fields] + kwargs.items())
+
 def instance_to_json(instance, *args, **kwargs):
     """
     Takes a model instance and returns the corresponding json object
     """
-    def instance_to_dict(i):
-        fields = [f.attname for f in instance._meta._fields()]
-        return dict([(f, instance.__dict__[f]) for f in fields] + kwargs.items())
-
-    d = instance_to_dict(instance)
+    d = instance_to_dict(instance, *args, **kwargs)
     d = dict([pair for pair in d.items() if type(pair[1]) != FieldFile])
     return json.dumps(d)
