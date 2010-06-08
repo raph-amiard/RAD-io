42db22742a09f9c6edf3837a1c9c8ac323f32f41
raph <raph@raph-laptop.(none)>
2010-06-08 08:15:04 +0200

    Corrected instance_to_dict function to not take FileFields

--- bin/rzz/utils/jsonutils.py
+++ bin/rzz/utils/jsonutils.py
@@ -4,12 +4,12 @@ from django.db.models.fields.files import FieldFile
 
 def instance_to_dict(instance, *args, **kwargs):
     fields = [f.attname for f in instance._meta._fields()]
-    return dict([(f, instance.__dict__[f]) for f in fields] + kwargs.items())
+    return dict([(f, instance.__dict__[f]) 
+              for f in fields 
+              if type(instance.__dict__[f]) != FieldFile] + kwargs.items())
 
 def instance_to_json(instance, *args, **kwargs):
     """
     Takes a model instance and returns the corresponding json object
     """
-    d = instance_to_dict(instance, *args, **kwargs)
-    d = dict([pair for pair in d.items() if type(pair[1]) != FieldFile])
-    return json.dumps(d)
+    return json.dumps(instance_to_dict(instance, *args, **kwargs))
