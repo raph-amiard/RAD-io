649d4478870ac321afe6c4711abc8853bb76eb0b
Raphael Amiard <raph@regina.(none)>
2010-06-08 15:55:38 +0200

    Added possibility to remove tags from audiofiles

--- share/media/js/uploadhandler.js
+++ share/media/js/uploadhandler.js
@@ -1,5 +1,30 @@
 var TOTAL_PLAYLIST_LENGTH = 0
 
+function autocomp_params_multi(list) {
+return {
+			minLength: 0,
+			source: function(request, response) {
+				// delegate back to autocomplete, but extract the last term
+				response($.ui.autocomplete.filter(list, extractLast(request.term)));
+			},
+			focus: function() {
+				// prevent value inserted on focus
+				return false;
+			},
+			select: function(event, ui) {
+				var terms = split( this.value );
+				// remove the current input
+				terms.pop();
+				// add the selected item
+				terms.push( ui.item.value );
+				// add placeholder to get the comma-and-space at the end
+				terms.push("");
+				this.value = terms.join(", ");
+				return false;
+			}
+		}
+}
+
 function split(val) {
     return val.split(/,\s*/);
 }
@@ -43,37 +68,32 @@ function handle_audiofiles_actions (e) {
     e.preventDefault();
     $.getJSON(this.href, function(data) {
         $('#audiofiles_actions_container').html(data.html);
-        availableTags = data.tag_list;
-        $('#id_tags').autocomplete({
-			minLength: 0,
-			source: function(request, response) {
-				// delegate back to autocomplete, but extract the last term
-				response($.ui.autocomplete.filter(availableTags, extractLast(request.term)));
-			},
-			focus: function() {
-				// prevent value inserted on focus
-				return false;
-			},
-			select: function(event, ui) {
-				var terms = split( this.value );
-				// remove the current input
-				terms.pop();
-				// add the selected item
-				terms.push( ui.item.value );
-				// add placeholder to get the comma-and-space at the end
-				terms.push("");
-				this.value = terms.join(", ");
-				return false;
-			}
-		});
+        $('#id_tags').autocomplete(autocomp_params_multi(data.tag_list));
+        $('#id_artist').autocomplete({source:data.artist_list});
+        $('.audiofile_tag_delete').click(function(e) {
+            e.preventDefault();
+            $audiofile_tag = $(this).parents('.audiofile_tag').first();
+            $.getJSON(this.href, function(response) {
+                console.log(response);
+                if (response.status == 'ok') {
+                    console.log(this);
+                    if($audiofile_tag.siblings().length == 0) {
+                        $audiofile_tag.parents('tr').first().remove();
+                    } else {
+                        $audiofile_tag.remove();
+                    }
+                }
+            });
+        });
         $('#audiofiles_actions_container form').ajaxForm({
+            dataType:'json',
             success: function(form_res) {
-                console.log(form_res);
-                $pl_element.html(new EJS({ url: '/site_media/js_templates/playlist_element.ejs'}).render(form_res));
+                // TODO: Add error handling
+                $('#audiofile_title', $pl_element).text(form_res.audiofile.title);
+                $('#audiofile_artist', $pl_element).text(form_res.audiofile.artist);
                 $('#audiofiles_actions_container').html('');
                 $('.audiofile_actions a').click(handle_audiofiles_actions);
-            },
-            dataType:'json'
+            }
         });
     });
 }
@@ -139,7 +159,6 @@ $(document).ready(function() {
     $('input[name="playlist_submit"]').click(function(e) {
         data = {'title' :$('input[name="playlist_title"]').val()};
         $.each($('#uploaded_audiofiles li input'), function(i, input) {data[i] = input.value;});
-        console.log(data);
         $.post(location.href,data, function(response) {
             alert('post successfull');
             alert(response);
