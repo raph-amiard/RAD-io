cc3f6443903b953758ca05296dbebaa284999a8c
raph <raph@raph-desktop.(none)>
2010-11-06 15:47:53 +0100

    Added ffmp3 stream player, appended it to the work in progress template

--- bin/rzz/radio_client/templatetags/radio_adress.py
+++ bin/rzz/radio_client/templatetags/radio_adress.py
@@ -9,3 +9,11 @@ def main_radio_adress(format_string):
         return "http://{0}/{1}.{2}.m3u".format(settings.RADIO_HOST, settings.RADIO_MOUNT_NAME, format_string)
     else:
         raise template.TemplateSyntaxError, "main_radio_adress tag requires a format either \"ogg\" or \"mp3\""
+
+
+@register.simple_tag
+def main_radio_adress_bare(format_string):
+    if format_string in ["ogg", "mp3"]:
+        return "http://{0}/{1}.{2}".format(settings.RADIO_HOST, settings.RADIO_MOUNT_NAME, format_string)
+    else:
+        raise template.TemplateSyntaxError, "main_radio_adress tag requires a format either \"ogg\" or \"mp3\""
