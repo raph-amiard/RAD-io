f23d786076207a7996c34725743a8b5de7709aa0
raph <raph.amiard@gmail.com>
2011-01-17 13:34:46 +0100

    Decoupled friends and artists app names and activation

--- bin/rzz/utils/context_processors.py
+++ bin/rzz/utils/context_processors.py
@@ -2,5 +2,9 @@ def radio_settings(request):
     from django.conf import settings
     return {
         'radio_short_name': settings.RADIO_SHORT_NAME,
-        'radio_long_name': settings.RADIO_LONG_NAME
+        'radio_long_name': settings.RADIO_LONG_NAME,
+        'is_friend_app_activated': settings.IS_FRIENDS_APP_ACTIVATED,
+        'is_artists_app_activated': settings.IS_ARTISTS_APP_ACTIVATED,
+        'artists_app_menu_name': settings.ARTISTS_APP_MENU_NAME,
+        'friends_app_menu_name': settings.FRIENDS_APP_MENU_NAME,
     }
