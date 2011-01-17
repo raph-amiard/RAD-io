030bd1e5638bb8c74719ec55308af8bd65ec2931
raph <raph.amiard@gmail.com>
2011-01-17 13:51:55 +0100

    Generic radio setttings

---
+++ bin/rzz/radio_settings.py
@@ -0,0 +1,18 @@
+##################
+# Radio settings #
+##################
+
+RADIO_SHORT_NAME = "RZZ"
+RADIO_LONG_NAME = "radiozerozero"
+RADIO_OUTPUTS = [
+    { 'format':'ogg', 'quality':8.0 },
+    { 'format':'mp3', 'bitrate':128 }
+]
+RADIO_MOUNT_NAME = "zero"
+# Jingle frequency in seconds
+RADIO_JINGLES_FREQUENCY = 20 * 60
+
+FRIENDS_APP_MENU_NAME = "Friends"
+ARTISTS_APP_MENU_NAME = "Artists"
+IS_FRIENDS_APP_ACTIVATED = True
+IS_ARTISTS_APP_ACTIVATED = True
