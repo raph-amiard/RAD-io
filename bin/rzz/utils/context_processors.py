de1611ae2e083c2d8f6cb5b2b8e1ca73cc657143
raph <raph.amiard@gmail.com>
2011-02-02 15:26:17 +0100

    Remove forgotten print

--- bin/rzz/utils/context_processors.py
+++ bin/rzz/utils/context_processors.py
@@ -2,7 +2,6 @@ from rzz import radio_settings as r_settings
 
 def radio_settings(request):
     d = {}
-    print dir(r_settings)
     for key in dir(r_settings):
         if not key.startswith("__"):
             d[key.lower()] = getattr(r_settings, key)
