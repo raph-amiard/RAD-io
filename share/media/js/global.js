2a9701d912c4f7d630b3b336b9b2c693131e61f6
raph <raph.amiard@gmail.com>
2011-02-05 11:02:13 +0100

    removed Array.prototype.remove

--- share/media/js/global.js
+++ share/media/js/global.js
@@ -224,9 +224,3 @@ object_with_keys = function(obj, keys) {
   }
   return new_obj;
 };
-Array.prototype.remove = function(from, to) {
-  var rest;
-  rest = this.slice((to || from) + 1 || this.length);
-  this.length = from < 0 ? this.length + from : from;
-  return this.push.apply(this, rest);
-};
