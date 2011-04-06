06fb25c324223ec8426d5906597dfa613b0da838
raph <raph.amiard@gmail.com>
2011-04-06 12:37:18 +0200

    More tweaaaking

--- bin/rzz/friends/models.py
+++ bin/rzz/friends/models.py
@@ -7,7 +7,7 @@ def friend_image_path(instance, filename):
 class Friend(models.Model):
 
     name = models.CharField('Friend name', max_length=200)
-    url = models.URLField(verify_exists=False, max_length=300)
+    url = models.URLField(verify_exists=False, max_length=300, null=True, blank=True)
     description = models.CharField('Artist biography', max_length=10000)
     picture = models.ImageField(upload_to=friend_image_path, null=True,blank=True)
 
